24ec7008a47d8c58f0a820afb052bff5fdd4b27a
DvirEzra <122629905+DvirEzra@users.noreply.github.com>
2023-07-22 14:50:11 +0300

    Update main.py
    fix Pytest

--- main.py
+++ main.py
@@ -1,72 +1,10 @@
 import requests
-import os
 import pytest
 
-def get_status_code(url):
+def test_website_status_code():
+    url = input("Enter the URL of the website you want to test: ")
     response = requests.get(url)
-    print(response.status_code)
-    return response.status_code
+    assert response.status_code == 200, f"Failed: Expected status code 200, but received {response.status_code}"
 
-def print_status_message(url):
-    status_code = get_status_code(url)
-    status_messages = {
-        100: "Continue",
-        101: "Switching Protocols",
-        200: "OK",
-        201: "Created",
-        202: "Accepted",
-        203: "Non-Authoritative Information",
-        204: "No Content",
-        205: "Reset Content",
-        206: "Partial Content",
-        300: "Multiple Choices",
-        301: "Moved Permanently",
-        302: "Found",
-        303: "See Other",
-        304: "Not Modified",
-        305: "Use Proxy",
-        307: "Temporary Redirect",
-        400: "Bad Request",
-        401: "Unauthorized",
-        402: "Payment Required",
-        403: "Forbidden",
-        404: "Not Found",
-        405: "Method Not Allowed",
-        406: "Not Acceptable",
-        407: "Proxy Authentication Required",
-        408: "Request Timeout",
-        409: "Conflict",
-        410: "Gone",
-        411: "Length Required",
-        412: "Precondition Failed",
-        413: "Request Entity Too Large",
-        414: "Request-URI Too Long",
-        415: "Unsupported Media Type",
-        416: "Requested Range Not Satisfiable",
-        417: "Expectation Failed",
-        500: "Internal Server Error",
-        501: "Not Implemented",
-        502: "Bad Gateway",
-        503: "Service Unavailable",
-        504: "Gateway Timeout",
-        505: "HTTP Version Not Supported"
-    }
-
-    if status_code in status_messages:
-        print("Status code", status_code, ":", status_messages[status_code])
-    else:
-        print("Unknown status code:", status_code)
-
-def test_print_status_message():
-    u = os.environ.get('URL')
-    u = 'https://' + u if u and not u.startswith('https://') else u
-
-    if u:
-        print(u)
-        print_status_message(u)
-        assert True  # If the code runs without exceptions, consider the test passed
-    else:
-        pytest.fail("URL is not set or is invalid")
-
-# Run the test function
-test_print_status_message()
+if __name__ == "__main__":
+    pytest.main([__file__])
