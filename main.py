66886b48a894208c1f0cf6146a35250db7316d5e
DvirEzra <122629905+DvirEzra@users.noreply.github.com>
2023-07-31 19:46:14 +0300

    Update main.py
    fix 31.07.23

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
+import os
 import requests
 import pytest
 
+def get_url():
+    url = os.environ.get("URL")
+    if url:
+        return url
+    else:
+        return input("Enter the URL of the website you want to test: ")
+
 def test_website_status_code():
-    url = input("Enter the URL of the website you want to test: ")
+    url = get_url()
     response = requests.get(url)
-    assert response.status_code == 200, f"Failed: Expected status code 200, but received {response.status_code}"
+    assert response.status_code == 200, "Failed: Expected status code 200, but received {}".format(response.status_code)
 
 if __name__ == "__main__":
     pytest.main([__file__])
