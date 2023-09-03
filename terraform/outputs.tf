107637797e518b1def362b4f11af2c1e3707c704
Omer Avrech <omeravrech@gmail.com>
2023-09-04 00:31:29 +0300

    Configure CD pipeline

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -3,32 +3,33 @@
 ###################
 
 output "backend_public_ip"{
-    value       = aws_instance.backend.public_ip
+    value       = aws_instance.ec2_backend_instance.public_ip
     description = "The public ip address assigned by AWS"
 }
 output "backend_private_ip" {
-    value       = aws_instance.backend.private_ip
+    value       = aws_instance.ec2_backend_instance.private_ip
     description = "The internal ip address assigned by AWS"
 }
-output "backend_key" {
+output "backend_private_key" {
     value       = tls_private_key.this[0].private_key_pem
     sensitive   = true
     description = "The private key to access the EC2 instance"
 }
+
 
 ####################
 # Frontend outputs #
 ####################
 
 output "frontend_public_ip"{
-    value       = aws_instance.frontend.public_ip
+    value       = aws_instance.ec2_frontend_instance.public_ip
     description = "The public ip address assigned by AWS"
 }
 output "frontend_private_ip"{
-    value       = aws_instance.frontend.private_ip
+    value       = aws_instance.ec2_frontend_instance.private_ip
     description = "The internal ip address assigned by AWS"
 }
-output "frontend_key" {
+output "frontend_private_key" {
     value       = tls_private_key.this[0].private_key_pem
     sensitive   = true
     description = "The private key to access the EC2 instance"
