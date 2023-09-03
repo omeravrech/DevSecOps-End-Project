b99b1d71cef563aaa869c1d7a4595c5d94d6a520
Omer Avrech <omeravrech@gmail.com>
2023-09-04 00:02:34 +0300

    fix terraform connectivity issue to ec2 instances

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,84 +1,108 @@
 provider "aws" {
-    region                  = "eu-central-1"
+    region                   = "eu-central-1"
+}
+resource "aws_vpc" "production_vpc" {
+    cidr_block               = "10.0.0.0/16"
+    enable_dns_support       = true
+    enable_dns_hostnames     = true
 }
 
-resource "aws_vpc" "production-vpc" {
-    cidr_block              = "10.0.0.0/16"
-    enable_dns_support      = true
-    enable_dns_hostnames    = true
+resource "aws_subnet" "subnet_backend" {
+    vpc_id                   = aws_vpc.production_vpc.id
+    cidr_block               = "10.0.1.0/24"
+    availability_zone        = "eu-central-1a"
+    map_public_ip_on_launch  = true
 }
 
-resource "aws_subnet" "frontend-subnet" {
-    vpc_id                  = aws_vpc.production-vpc.id
-    cidr_block              = "10.0.1.0/24"
-    map_public_ip_on_launch = true
-}
-resource "aws_subnet" "backend-subnet" {
-    vpc_id                  = aws_vpc.production-vpc.id
-    cidr_block              = "10.0.2.0/24"
-    map_public_ip_on_launch = true
+resource "aws_subnet" "subnet_frontend" {
+    vpc_id                   = aws_vpc.production_vpc.id
+    cidr_block               = "10.0.2.0/24"
+    availability_zone        = "eu-central-1a"
+    map_public_ip_on_launch  = true
 }
 
-resource "aws_security_group" "sg-ter-1" {
-    vpc_id = aws_vpc.production-vpc.id
+resource "aws_internet_gateway" "default_gw" {
+    vpc_id                   = aws_vpc.production_vpc.id
+}
 
-    ingress  {
-        description         = "allow HTTP connectivity to vpc"
-        from_port           = 80
-        to_port             = 80
-        protocol            = "tcp"
-        cidr_blocks         = [aws_subnet.backend-subnet.cidr_block, aws_subnet.frontend-subnet.cidr_block]
+resource "aws_default_route_table" "production_route_table" {
+    default_route_table_id   = aws_vpc.production_vpc.default_route_table_id
+    route {
+        cidr_block           = "0.0.0.0/0"
+        gateway_id           = aws_internet_gateway.default_gw.id
+    }
+    route {
+        cidr_block           = "10.0.0.0/16"
+        gateway_id           = "local"
+    }
+}
+
+resource "aws_security_group" "sg_main_1" {
+    vpc_id                   = aws_vpc.production_vpc.id
+
+    ingress {
+        description          = "allow ping to all instances"
+        from_port            = 8
+        to_port              = 0
+        protocol             = "icmp"
+        cidr_blocks          = ["0.0.0.0/0"]
+
     }
     ingress {
-        description         = "allow ssh connectivity to vpc"
-        from_port           = 22
-        to_port             = 22
-        protocol            = "tcp"
-        cidr_blocks         = [aws_vpc.production-vpc.cidr_block]
+        description          = "allow ssh connectivity to vpc"
+        from_port            = 22
+        to_port              = 22
+        protocol             = "tcp"
+        cidr_blocks          = ["0.0.0.0/0"]
     }
-
+    ingress {
+        description          = "allow HTTP connectivity to vpc"
+        from_port            = 80
+        to_port              = 80
+        protocol             = "tcp"
+        cidr_blocks          = ["0.0.0.0/0"]
+    }
     egress {
-        description         = "Allow all exit traffic"
-        from_port           = 0
-        to_port             = 0
-        protocol            = "-1"
-        cidr_blocks         = ["0.0.0.0/0"]
+        description          = "Allow all exit traffic"
+        from_port            = 0
+        to_port              = 0
+        protocol             = "-1"
+        cidr_blocks          = ["0.0.0.0/0"]
     }
  }
 
 resource "tls_private_key" "this" {
-    count                   = 2
-    algorithm               = "RSA"
-    rsa_bits                = 2048
+    count                    = 2
+    algorithm                = "RSA"
+    rsa_bits                 = 2048
 }
 
-
-resource "aws_key_pair" "backend-key-pair" {
-    count                   = 1
-    key_name                = "backend-public-key"
-    public_key              = trimspace(tls_private_key.this[0].public_key_openssh)
+resource "aws_key_pair" "backend_key_pair" {
+    count                    = 1
+    key_name                 = "backend_public_key"
+    public_key               = trimspace(tls_private_key.this[0].public_key_openssh)
 }
 
-resource "aws_key_pair" "frontend-key-pair" {
-    count                   = 1
-    key_name                = "frontend-public-key"
-    public_key              = trimspace(tls_private_key.this[1].public_key_openssh)
+resource "aws_key_pair" "frontend_key_pair" {
+    count                    = 1
+    key_name                 = "frontend_public_key"
+    public_key               = trimspace(tls_private_key.this[1].public_key_openssh)
 }
 
-resource "aws_instance" "backend" {
-    ami                     = "ami-04e601abe3e1a910f"
-    instance_type           = "t2.micro"
-    availability_zone       = "eu-central-1a"
-    subnet_id               = aws_subnet.backend-subnet.id
-    security_groups         = [aws_security_group.sg-ter-1.id]
-    key_name                = aws_key_pair.backend-key-pair[0].key_name
+resource "aws_instance" "ec2_backend_instance" {
+    ami                      = "ami-04e601abe3e1a910f"
+    instance_type            = "t2.micro"
+    availability_zone        = "eu-central-1a"
+    subnet_id               = aws_subnet.subnet_backend.id
+    security_groups         = [aws_security_group.sg_main_1.id]
+    key_name                 = aws_key_pair.backend_key_pair[0].key_name
 }
 
-resource "aws_instance" "frontend" {
-    ami                     = "ami-04e601abe3e1a910f"
-    instance_type           = "t2.micro"
-    availability_zone       = "eu-central-1a"
-    subnet_id               = aws_subnet.frontend-subnet.id
-    security_groups         = [aws_security_group.sg-ter-1.id]
-    key_name                = aws_key_pair.frontend-key-pair[0].key_name
+resource "aws_instance" "ec2_frontend_instance" {
+    ami                      = "ami-04e601abe3e1a910f"
+    instance_type            = "t2.micro"
+    availability_zone        = "eu-central-1a"
+    subnet_id               = aws_subnet.subnet_frontend.id
+    security_groups         = [aws_security_group.sg_main_1.id]
+    key_name                 = aws_key_pair.frontend_key_pair[0].key_name
 }
